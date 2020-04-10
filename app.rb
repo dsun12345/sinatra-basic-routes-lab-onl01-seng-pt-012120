0eeb46be983bd3c27733c14332a79063735b9523
Daniel Sun <danielsun12345@gmail.com>
2020-04-10 17:28:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    
+    
+  end 
+  
+  
+  get '/' do 
+    
+  
+  
+  end
+  
+  
+  get '/' do 
+    
+    
+    
+    
+  end 
+  
+  
+  
+  
 end
